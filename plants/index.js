6a9e7e5d4ba989cd783c91b681053dc9d170d0f2
amrkov5 <amrkov5@gmail.com>
2023-04-05 20:58:43 +0300

    feature: working on hamb menu

--- plants/index.js
+++ plants/index.js
@@ -1,5 +1,14 @@
-const hamburger = document.getElementsByClassName('hamb-container')
+const hambContainer = document.querySelector('.hamb-container')
+const hambLine = document.querySelector('.hamb-line')
+const hambBg = document.querySelector('.bg-menu')
+const hambMenu = document.querySelector('.hamb-menu')
 
-addEventListener('click', () => {
-    hamburger
+hambContainer.addEventListener('click', (event) => {
+    console.log(event)
+    hambMenu.classList.toggle('hidden-menu')
+    hambBg.classList.toggle('hidden-bg')
 })
+
+// hambMenu.addEventListener('animationend', () => {
+//     hambBg.classList.toggle('hidden-bg')
+// })
