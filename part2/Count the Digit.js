19505002f1d8f029e47a65547f5323a7a9d90542
amrkov5 <amrkov5@gmail.com>
2023-04-15 15:39:54 +0300

    solved katas

--- part2/Count the Digit.js
+++ part2/Count the Digit.js
@@ -2,14 +2,11 @@ function nbDig(n, d) {
     result = 0
     for (let k = 0; k<= n; k++) {
         let square = k*k
-        if (square.toString().split('').filter( (item) => item == d).length > 0) {
             result = result + square.toString().split('').filter( (item) => item == d).length
-            console.log(result)
-        }
     }
     // m = 0
     // console.log(m.toString().split('').filter( (item) => item == d).length)
-    // console.log(result)
+    console.log(result)
   }
 
   let n = 5750
---
+++ part2/Find the missing element.js
@@ -0,0 +1,10 @@
+function findMissing(arr1, arr2) {
+    arr1.forEach(element => {
+        if (arr1.filter((el) => {element == el}).length == 0 == ) {
+            console.log(element)
+        }
+    });
+}
+
+arr1 = [1, 2, 3]
+arr2 = [1, 3]
